4fb2d30ea91d3bbb035725acc40c8029f0f92ee1
Christos Tranoris <tranoris@gmail.com>
2017-02-22 20:48:13 +0200

    changes

--- anothertest/anothertest.ino
+++ anothertest/anothertest.ino
@@ -41,6 +41,7 @@ static const u1_t PROGMEM APPSKEY[16] = { 0x80, 0x48, 0xCA, 0xFC, 0x3F, 0x5C, 0x
 // See http://thethingsnetwork.org/wiki/AddressSpace
 static const u4_t DEVADDR = 0x26011F1E ; // <-- Change this address for every node!
 
+
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
 // DISABLE_JOIN is set in config.h, otherwise the linker will complain).
@@ -48,10 +49,12 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
+static uint8_t mydata[] = "Hello, world!";
+static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 5; //60
+const unsigned TX_INTERVAL = 10;
 
 // Pin mapping
 const lmic_pinmap lmic_pins = {
@@ -61,125 +64,93 @@ const lmic_pinmap lmic_pins = {
     .dio = {2, 6, 7},
 };
 
-
-#define ss Serial
-byte dataoutgoing[50];
-
-static uint8_t mydata[] = "Hello: ";
-static osjob_t sendjob;
-int counter = 100;
-
 void onEvent (ev_t ev) {
-    ss.print(os_getTime());
-    ss.print(": ");
+    Serial.print(os_getTime());
+    Serial.print(": ");
     switch(ev) {
         case EV_SCAN_TIMEOUT:
-            ss.println(F("EV_SCAN_TIMEOUT"));
+            Serial.println(F("EV_SCAN_TIMEOUT"));
             break;
         case EV_BEACON_FOUND:
-            ss.println(F("EV_BEACON_FOUND"));
+            Serial.println(F("EV_BEACON_FOUND"));
             break;
         case EV_BEACON_MISSED:
-            ss.println(F("EV_BEACON_MISSED"));
+            Serial.println(F("EV_BEACON_MISSED"));
             break;
         case EV_BEACON_TRACKED:
-            ss.println(F("EV_BEACON_TRACKED"));
+            Serial.println(F("EV_BEACON_TRACKED"));
             break;
         case EV_JOINING:
-            ss.println(F("EV_JOINING"));
+            Serial.println(F("EV_JOINING"));
             break;
         case EV_JOINED:
-            ss.println(F("EV_JOINED"));
+            Serial.println(F("EV_JOINED"));
             break;
         case EV_RFU1:
-            ss.println(F("EV_RFU1"));
+            Serial.println(F("EV_RFU1"));
             break;
         case EV_JOIN_FAILED:
-            ss.println(F("EV_JOIN_FAILED"));
+            Serial.println(F("EV_JOIN_FAILED"));
             break;
         case EV_REJOIN_FAILED:
-            ss.println(F("EV_REJOIN_FAILED"));
+            Serial.println(F("EV_REJOIN_FAILED"));
             break;
             break;
         case EV_TXCOMPLETE:
-            ss.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
             if(LMIC.dataLen) {
                 // data received in rx slot after tx
-                ss.print(F("---------------------------------------------Data Received: "));
-                ss.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
-                ss.println();
+                Serial.print(F("Data Received: "));
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
             }
             // Schedule next transmission
             os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
             break;
         case EV_LOST_TSYNC:
-            ss.println(F("EV_LOST_TSYNC"));
+            Serial.println(F("EV_LOST_TSYNC"));
             break;
         case EV_RESET:
-            ss.println(F("EV_RESET"));
+            Serial.println(F("EV_RESET"));
             break;
         case EV_RXCOMPLETE:
             // data received in ping slot
-            ss.println(F("EV_RXCOMPLETE"));
+            Serial.println(F("EV_RXCOMPLETE"));
             break;
         case EV_LINK_DEAD:
-            ss.println(F("EV_LINK_DEAD"));
+            Serial.println(F("EV_LINK_DEAD"));
             break;
         case EV_LINK_ALIVE:
-            ss.println(F("EV_LINK_ALIVE"));
+            Serial.println(F("EV_LINK_ALIVE"));
             break;
          default:
-            ss.println(F("Unknown event"));
+            Serial.println(F("Unknown event"));
             break;
     }
 }
 
 void do_send(osjob_t* j){
-
-    bool newData = false;
-    unsigned long chars;
-    unsigned short sentences, failed;
-
-    String message;
-     
-    ss.println("LAT = no newData.");
-    message = "HELLO";
-     
-    
-    //message = "M"+String(counter) + message;
-    message.getBytes(dataoutgoing, message.length()+1);
-    counter++;
-    if (counter>=999){
-      counter = 100;
-    }
-    
     // Check if there is not a current TX/RX job running
     if (LMIC.opmode & OP_TXRXPEND) {
-        ss.println(F("OP_TXRXPEND, not sending"));
+        Serial.println(F("OP_TXRXPEND, not sending"));
     } else {
-
-        LMIC_setTxData2(1, (uint8_t*) dataoutgoing, message.length() , 0);
-        ss.println("Sending msg: "+message);
-        ss.println(F("Packet queued: "));
-        for (int i = 0; i < message.length(); i++) {
-                ss.print((char)dataoutgoing[i]);
-        }
-        ss.println(F("->"));        
-      digitalWrite( 8, HIGH);
-      delay(500);              // wait for a second
-      digitalWrite( 8, LOW);      
-        
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 
 void setup() {
-    pinMode( 8, OUTPUT); 
-    
-    Serial.begin(9600);    
-    ss.begin(9600);
-    ss.println(F("Starting"));
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
 
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
 
     // LMIC init
     os_init();
@@ -237,4 +208,3 @@ void setup() {
 void loop() {
     os_runloop_once();
 }
-
